25c17ff965548267809f1891b5f7f4ebde1a6952
doubsman <doubsman@gmail.com>
2020-01-28 16:15:29 +0100

    0.2 operation add, move, delete characters

--- FolderREname_old.py
+++ FolderREname_old.py
@@ -14,75 +14,76 @@ class ManageFolders(QObject):
 	def __init__(self, pathfolder, parent=None):
 		"""Init."""
 		super(ManageFolders, self).__init__(parent)
-		self.parent = parent
 		self.pathfolder = pathfolder
+		self.parent = parent
 		# build list
-		self.pathList = self.folders_list()
+		self.pathList = self.list_folders(pathfolder)
 		self.backList = self.pathList
 		self.cleanlist = []
-		# log
+		self.counter = 0
 		self.logFileName = QDateTime.currentDateTime().toString('yyMMddhhmmss') + "_ManageFolders.log"
 		self.logFileName = path.join(path.dirname(path.abspath(__file__)), "LOG", self.logFileName)
 
 	def folders_rename(self):
 		"""Rename folders list and write log."""
 		self.write_log_file('START OPERATIONS', self.pathfolder, False)
-		counter = 0
+		self.counter = 0
 		for folderName in self.backList:
 			# clean name
-			self.write_log_file('FOLDER FOLDER', folderName)
-			self.write_log_file('CLEAN FOLDER', self.cleanlist[counter])
-			rename(path.join(self.pathfolder, folderName), path.join(self.pathfolder, self.cleanlist[counter]))
-			counter += 1
+			self.write_log_file('FOLDER NAME', folderName)
+			self.write_log_file('CLEAN NAME', self.cleanlist[self.counter])
+			#rename(path.join(pathList, self.folderName), path.join(pathList, self.cleanName))
 		self.write_log_file('END OPERATIONS.',"", False)
 		startfile(self.logFileName)
 
 	def folders_control(self):
-		counter = 0
+		self.counter = 0
 		for folderName in self.backList:
-			print(folderName)
-			print(self.cleanlist[counter]+"\n")
-			counter += 1
-
-	def folders_list(self):
-		"""Build list folders."""
-		listdirs = []
-		for _, listdirs, _ in walk(self.pathfolder):
-			break
-		return listdirs
-
-	def replace_characters(self, startC, goalC):
+				print(folderName)
+				print(self.cleanlist[self.counter]+"\n")
+				self.counter += 1
+	
+	def folders_replace_character(self, startC, goalC):
 		self.cleanlist = []
 		for folderName in self.pathList:
 			self.cleanlist.append(folderName.replace(startC, goalC))
 		self.pathList = self.cleanlist
 
-	def add_characters(self, wordadd, goalC, decoCL ="", decoCR =""):
+	def folders_add_character(self, wordC, goalC, decoCL ="", decoCR =""):
 		self.cleanlist = []
+		if isinstance(goalC, str):
+			if goalC == 'max':
+				goalC = int(len(word))
 		for folderName in self.pathList:
-			if isinstance(goalC, str):
-				if goalC == 'max':
-					goalC = int(len(folderName))
-			self.cleanlist.append(folderName[:goalC] + decoCL + wordadd + decoCR + folderName[goalC:])
+			self.cleanlist.append(folderName[:goalC] + decoCL + wordC + decoCR + folderName[goalC:])
 		self.pathList = self.cleanlist
 
-	def delete_characters(self, startC, lenghtC):
+	def folders_delete_characters(self, startC, lenghtC):
 		self.cleanlist = []
 		for folderName in self.pathList:
 			self.cleanlist.append(folderName[:startC] + folderName[lenghtC + startC:])
 		self.pathList = self.cleanlist
 
-	def move_characters(self, startC, lenghtC, goalC, decoCL ="", decoCR =""):
+	def folders_move_characters(self, startC, lenghtC, goalC, decoCL ="", decoCR =""):
 		self.cleanlist = []
 		for folderName in self.pathList:
-			if isinstance(goalC, str):
-				if goalC == 'max':
-					goalC = int(len(folderName))
-			moveC = folderName[startC:lenghtC]
-			tempC = folderName[:startC] + folderName[startC + lenghtC:]
-			self.cleanlist.append(tempC[:goalC] + decoCL + moveC.strip() + decoCR + tempC[goalC:])
+			self.cleanlist.append(self.func_mov_characters(folderName, startC, lenghtC, goalC, decoCL , decoCR))
 		self.pathList = self.cleanlist
 	
+	def func_mov_characters(self, word, startC, lenghtC, goalC, decoCL ="", decoCR =""):
+		moveC = word[startC:lenghtC]
+		tempC = word[:startC] + word[startC + lenghtC:]
+		if isinstance(goalC, str):
+			if goalC == 'max':
+				goalC = int(len(word))
+		return tempC[:goalC] + decoCL + moveC.strip() + decoCR + tempC[goalC:]
+
+	def list_folders(self, path):
+		"""Build list folders."""
+		for _, dirs, _ in walk(path):
+			break
+		return dirs
+
 	def write_log_file(self, operation, line, modification = True, writeconsole = True):
 		"""Write log file."""
 		if modification:
@@ -98,6 +99,7 @@ class ManageFolders(QObject):
 		if writeconsole:
 			print(logline)
 
+
 if __name__ == '__main__':
 	app = QApplication(argv)
 	if len(argv)>1:
@@ -105,15 +107,16 @@ if __name__ == '__main__':
 		myfolder = argv[1]
 	else:
 		# test envt
-		myfolder = "T:\\work\\Fila Brazillia NT\\\Singles & EPs"
-	# build class process
+		myfolder = "T:\\work\\Fila Brazillia NT\\\Compilations"
+	# class
 	BuildProcess = ManageFolders(myfolder)
-	BuildProcess.replace_characters('(', '[')
-	BuildProcess.replace_characters(')', ']')
-	BuildProcess.replace_characters(' [Web]', '')
-	BuildProcess.move_characters(0, 5, 'max',' (',')')
-	#BuildProcess.add_characters('VA - ',0)
-	#BuildProcess.delete_characters(5,3)
+	BuildProcess.folders_replace_character('(', '[')
+	BuildProcess.folders_replace_character(')', ']')
+	BuildProcess.folders_replace_character(' [Web]', '')
+	BuildProcess.folders_move_characters(0, 5, 'max',' (',')')
+	BuildProcess.folders_add_character('VA - ',0)
+	#BuildProcess.folders_delete_characters(5,3)
 	BuildProcess.folders_control()
-	# processing
 	BuildProcess.folders_rename()
+	# download list
+	#BuildProcess.processManageFolders(myfolder)
